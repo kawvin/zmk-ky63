5d320c5d930340f1924fd25a6471fdaf59511050
kawvin <285781427@qq.com>
2024-10-21 23:22:08 +0800

    layer

--- boards/shields/display-042/widgets/layer_status.c
+++ boards/shields/display-042/widgets/layer_status.c
@@ -93,9 +93,9 @@ static void set_layer_symbol(lv_obj_t *label, struct layer_status_state state) {
         lv_label_set_text(label, text);
     }
     
-    // lv_obj_center(label); //居中
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-    // lv_obj_center(label);
+    //lv_obj_center(label); //居中
+    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_center(label);
     // lv_obj_set_style_transform_angle(label, 50, 0);f
 
 }
@@ -120,9 +120,9 @@ ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 
 int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent) {
     widget->obj = lv_label_create(parent);
-    lv_obj_set_size(widget->obj,  LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    //lv_obj_set_size(widget->obj,  LV_SIZE_CONTENT, LV_SIZE_CONTENT);
     //lv_obj_align(widget->obj, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_obj_set_size(widget->obj, 72, 18);
+    lv_obj_set_size(widget->obj, 72, 14);
     lv_obj_align(widget->obj, LV_ALIGN_CENTER, 0, 0);
     
     sys_slist_append(&widgets, &widget->node);
