b1f9baf37a23e26aa8c5cb2b63bede855f15440e
kawvin <285781427@qq.com>
2024-10-22 14:50:15 +0800

    电池

--- boards/shields/display-042/widgets/battery_status.c
+++ boards/shields/display-042/widgets/battery_status.c
@@ -74,36 +74,36 @@ static void set_battery_symbol(lv_obj_t *widget, struct battery_state state) {
 
     // lv_canvas_draw_rect(canvas, 0, 2, 29, 12, &rect_white_dsc);
     // lv_canvas_draw_rect(canvas, 1, 3, 27, 10, &rect_black_dsc);
-/*
+
     if (!state.usb_present) {
         lv_draw_img_dsc_t img_dsc;
         lv_draw_img_dsc_init(&img_dsc); //x,y是坐标，src是图像的源，可以是文件、结构体指针、Symbol，img_dsc是图像的样式。
-        //lv_canvas_draw_img(canvas, 0, 0, batterys_level[0], &img_dsc);
+        lv_canvas_draw_img(canvas, 0, 0, batterys_level[0], &img_dsc);
         // lv_img_set_src(symbol, batterys_level[0]);
         if (level > 96) {
-            lv_canvas_draw_rect(canvas, 2, 5, 6, 12, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 5, 7, 12, &rect_white_dsc);
         } else if (level > 88) {
-            lv_canvas_draw_rect(canvas, 2, 6, 6, 11, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 6, 7, 11, &rect_white_dsc);
         } else if (level > 80) {
-            lv_canvas_draw_rect(canvas, 2, 7, 6, 10, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 7, 7, 10, &rect_white_dsc);
         } else if (level > 72) {
-            lv_canvas_draw_rect(canvas, 2, 8, 6, 9, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 8, 7, 9, &rect_white_dsc);
         } else if (level > 64) {
-            lv_canvas_draw_rect(canvas, 2, 9, 6, 8, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 9, 7, 8, &rect_white_dsc);
         } else if (level > 56) {
-            lv_canvas_draw_rect(canvas, 2, 10, 6, 7, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 10, 7, 7, &rect_white_dsc);
         } else if (level > 48) {
-            lv_canvas_draw_rect(canvas, 2, 11, 6, 6, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 11, 7, 6, &rect_white_dsc);
         } else if (level > 40) {
-            lv_canvas_draw_rect(canvas, 2, 12, 6, 5, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 12, 7, 5, &rect_white_dsc);
         } else if (level > 32) {
-            lv_canvas_draw_rect(canvas, 2, 13, 6, 4, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 13, 7, 4, &rect_white_dsc);
         } else if (level > 24) {
-            lv_canvas_draw_rect(canvas, 2, 14, 6, 3, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 14, 7, 3, &rect_white_dsc);
         } else if (level > 16) {
-            lv_canvas_draw_rect(canvas, 2, 15, 6, 2, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 15, 7, 2, &rect_white_dsc);
         } else if (level > 8) {
-            lv_canvas_draw_rect(canvas, 2, 16, 6, 1, &rect_white_dsc);
+            lv_canvas_draw_rect(canvas, 3, 16, 7, 1, &rect_white_dsc);
         } else {
             
         }
@@ -111,7 +111,6 @@ static void set_battery_symbol(lv_obj_t *widget, struct battery_state state) {
     } else {
         lv_img_set_src(canvas, batterys_level[1]);
     }
-*/
 }
 
 void battery_status_update_cb(struct battery_state state) {
@@ -193,21 +192,21 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 13, 20, LV_IMG_CF_TRUE_COLOR);
         lv_obj_align(image_canvas, LV_ALIGN_TOP_LEFT, i*15, 1);
 
-        lv_draw_rect_dsc_t rect_black_dsc;
-        init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
-        lv_draw_rect_dsc_t rect_white_dsc;
-        init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
-        lv_draw_img_dsc_t img_dsc;
-        lv_draw_img_dsc_init(&img_dsc); //x,y是坐标，src是图像的源，可以是文件、结构体指针、Symbol，img_dsc是图像的样式。
-        lv_canvas_draw_img(image_canvas, 0, 0, batterys_level[0], &img_dsc);
-        lv_canvas_draw_rect(image_canvas, 2, 12, 6, 5, &rect_white_dsc);
+        // lv_draw_rect_dsc_t rect_black_dsc;
+        // init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
+        // lv_draw_rect_dsc_t rect_white_dsc;
+        // init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
+        // lv_draw_img_dsc_t img_dsc;
+        // lv_draw_img_dsc_init(&img_dsc); //x,y是坐标，src是图像的源，可以是文件、结构体指针、Symbol，img_dsc是图像的样式。
+        // lv_canvas_draw_img(image_canvas, 0, 0, batterys_level[0], &img_dsc);
+        // lv_canvas_draw_rect(image_canvas, 2, 12, 9, 5, &rect_white_dsc);
 
         // lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, i*14+3, 10);
         
         // lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
         // lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
 
-        //lv_img_set_src(image_canvas, batterys_level[2]);
+        lv_img_set_src(image_canvas, batterys_level[0]);
     }
 #endif
 
